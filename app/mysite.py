4dcbf5b651572ccc2235e2fa764f15bf583f9eb3
Riley Rustad <rileyrustad@gmail.com>
2016-05-31 18:40:35 -0700

    initial commit

---
+++ app/mysite.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Length
+import json
+import sys
+sys.path.append('/Users/mac28/src/website/pipeline')
+
+from predictor import predict
+dict90 = json.load(open('../pipeline/data/Day90ApartmentData.json'))
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+boostrap = Bootstrap(app)
+
+class NameForm(Form):
+    id_number = StringField('What is the listing ID Number?',
+    	validators=[Required(), 
+    	Length(10, message = 'Field must be 10 digit craigslist ID Number')])
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/scorer', methods=['GET', 'POST'])
+def scorer():
+	id_number = None
+	form = NameForm()
+	my_dict = None
+	pred, diff, no_assumptions, assumptions, title = (None, 
+		None, None, None, None)
+
+
+	if form.validate_on_submit():
+		id_number = form.id_number.data
+		form.id_number.data = ''
+		pred, diff, no_assumptions, assumptions,title = predict(
+			id_number, dict90,'../pipeline/data/')
+		my_dict = assumptions
+		
+
+	attrs = ['bed','bath','feet','dog','cat','content',
+	'getphotos','hasmap','housingtype','lat','long','laundry',
+	'parking','price','smoking','wheelchair']
+
+	
+	
+	return render_template('scorer.html', id_number=id_number, attrs=attrs,
+		my_dict=my_dict,form=form,pred=pred, diff=diff, 
+		no_assumptions=no_assumptions,assumptions = assumptions,title=title)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pipeline/predictor.py
@@ -0,0 +1,80 @@
+import pickle
+from scrape import info
+import pandas as pd
+from pandas import DataFrame
+from processing import imputer, imputables_getter
+import json
+from modeler import clusterer
+
+
+def predict(listing, my_dict, path):
+	''' Predict the price of an apartment listing.
+	Parameters
+	----------
+	listing: Craigslist ID number for the individual listing.
+
+	my_dict: Dictionary containing data for listings in the last 90 days
+
+	path: pathway to data file from where the commans is called
+
+	Returns
+	-------
+	pred = prediction price of 
+
+
+	'''
+
+
+	# Pull the info for a specific listing.
+	my_info1 = info(listing,{})
+	my_info = my_info1.copy()
+	
+	
+
+	reg = pickle.load(open(path+'sklearn-model.pkl',"rb" ))
+
+
+	# dframe = dframe[dframe.lat > 45.4][dframe.lat < 45.6][dframe.long < -122.0][dframe.long > -123.5]
+
+	imputables = imputables_getter(my_dict)
+
+	# impute any missing data.
+	my_info = imputer(my_info[listing], imputables)
+
+	dframe = DataFrame({listing:my_info}).T[['content', 'laundry',  'price', 
+	'dog', 'bed', 'bath', 'feet', 'long', 'parking', 'lat', 'smoking', 
+	'getphotos', 'cat', 'hasmap', 'wheelchair', 'housingtype']]
+
+
+	neighborhoods = pickle.load(open(path+'neighborhoodlist.pkl', "rb"))
+
+	# Assign our listing to its closest neighborhood
+	dframe['neighborhood'] = clusterer(
+		dframe['lat'][listing],dframe['long'][listing],neighborhoods)
+
+	columns = pickle.load(open(path+'columns.pkl', "rb"))
+
+	# Get rid of unused columns
+	for row in dframe:
+		if row not in columns:
+			dframe = dframe.drop(row, axis = 1)
+	for column in columns:
+		if column not in dframe:
+			dframe[column] = 0
+
+	# Standardize the column order for the model
+	dframe = dframe[columns]
+
+	price = my_info['price']
+	title = my_info1[listing]['title']
+
+	pred = reg.predict(dframe.drop('price',axis=1))
+	diff = price - pred
+	return pred[0], diff[0], my_info1, my_info, title
+
+if __name__ == '__main__':
+	listing = '5580713155'
+	with open('data/Day90ApartmentData.json') as f:
+	    my_dict = json.load(f)
+	print predict(listing, my_dict, 'data/')
+
